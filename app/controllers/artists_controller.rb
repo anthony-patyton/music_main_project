081c010f4fd8090a22df0af637d488000a1d4e60
John Doe <paytonanthony2020@gmail.com>
2021-06-03 14:31:45 -0600

    Added all parent and child controllers and created the components

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,13 +1,66 @@
 class ArtistsController < ApplicationController
+  # follow the pattern
+  # change Model_name -> @parent.model_names
+  # before_action :set_parent
+  # private
+  #   def set_parent
+  #     @parent = Parent.find(params[:id])
+  #   end
+  before_action :set_bill_board
+  before_action :set_artist, only [:show, :edit, :update, destroy]
+
   def index
+    @artists = @bill_board.artists
+    render component: 'Artists', props: { artists: @artists, bill_board: @bill_board }
   end
 
   def show
+    render component: 'Artist', props: { artist: @artist, bill_board: @billboard }
   end
 
   def new
+    @artist = @bill_board.artists.new
+    render component: 'ArtistNew', props: { artist: @artist, bill_board: @bill_board }
   end
 
-  def edit
+  def create
+    @artist = @bill_board.artists.new(artist_params)
+    if @artist.save
+      redirect_to root_path
+      # redirect_to bill_board_artists_path(params[:id])
+    else
+      render component: 'ArtistNew', props: { artist: @artist, bill_board: @bill_board }
+    end
   end
+  
+  def edit
+    render component: 'ArtistEdit', props: { artist: @artist, bill_board: @bill_board }
+  end
+
+  def update
+    if @artist.update(artist_params)
+      redirect_to root_path
+    else
+      render component: 'ArtistEdit', props: { artist: @artist, bill_board: @bill_board}
+    end
+  end
+
+  def destroy
+    @artist.destroy
+    redirect_to root_path
+  end
+  
+  private
+    def set_bill_board
+      @bill_board = BillBoard.find(params[:bill_board_id])
+    end
+
+    def set_artist
+      @artist = Artist.find(params[:id])
+      # @artist = @bill_board.artists.find(params[:id])
+    end
+
+    def artist_params
+      params.require(:artist).permit(:name, :about, :age)
+    end
 end
--- app/controllers/bill_boards_controller.rb
+++ app/controllers/bill_boards_controller.rb
@@ -1,13 +1,58 @@
 class BillBoardsController < ApplicationController
+  
+  before_action :set_bill_board, only: [:show, :edit, :update]
   def index
+    @bill_boards = BillBoard.all
+    render component: 'BillBoards', props: { bill_boards: @bill_boards }
   end
-
+  
   def show
+    render component: 'BillBoard', props: { bill_board: @bill_board }
   end
 
   def new
+    @bill_board = BillBoard.new
+    render component: 'BillBoardNew', props: { bill_board: @bill_board }
   end
 
   def edit
+    if @bill_board.save
+      redirect_to root_path
+    else
+      render component: 'BillBoardEdit', props: { bill_board: @bill_board}
+    end
   end
+
+  def create
+    @bill_board = BillBoard.new(bill_board_params)
+    if @bill_board.save
+      redirect_to root_path
+    else
+      render component: 'BillBoardNew', props: { bill_board: @bill_board }
+    end
+  end
+
+  def update
+    if @bill_board.update(bill_board_params)
+      redirect_to root_path
+    else
+      render component: 'BillBoardEdit', props: { bill_board: @bill_board }
+    end
+  end
+
+  def destroy
+    BillBoard.find(params[:id]).destroy
+    redirect_to root_path
+  end
+
+  private
+  # { bill_board: { title: 'ksjdflaj;s'}}
+  # check data and protects from sql injection SELECT*FROMUSER's
+    def bill_board_params
+      params.require(:bill_board).permit(:title, :body)
+    end
+
+    def set_bill_board
+      @bill_board = BillBoard.find(params[:id])
+    end
 end
